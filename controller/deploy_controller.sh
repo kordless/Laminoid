7cd866e7a7191e9e4adc7118d773f285847a5c5a
Kord Campbell <kordless@gmail.com>
2023-11-27 15:38:05 -0600

    change to mitta.ai

--- controller/deploy_controller.sh
+++ controller/deploy_controller.sh
@@ -3,7 +3,7 @@ TYPE=e2-medium
 NAME=controller
 NEW_UUID=$(LC_ALL=C tr -dc 'a-z0-9' </dev/urandom | head -c 4 ; echo)
 
-IP=34.122.236.144
+IP=34.170.34.122
 
 PREEMPTIBLE=" \
 --maintenance-policy=TERMINATE \
